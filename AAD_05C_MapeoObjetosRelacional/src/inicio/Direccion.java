cffcc95c1d2229044fc06be9f8e6e1492950a3f7
Miguel A. Simeón <miguelsimeon@gmail.com>
2017-01-29 21:11:52 +0100

    ADD - Actividad 5C - Hibernate + mapeado de objetos+relación 1-1, 1-n

---
+++ AAD_05C_MapeoObjetosRelacional/src/inicio/Direccion.java
@@ -0,0 +1,61 @@
+package inicio;
+
+public class Direccion {
+
+	private String calle;
+	private String pob;
+	private int cp;
+	private int id;
+	private Empresa emp;
+
+	public Direccion(String c, String p, int cp) {
+		this.calle=c;
+		this.pob=p;
+		this.cp = cp;
+	}
+	
+	public String getDirección(){
+		return calle+", CP: "+String.valueOf(cp)+" ("+pob+")";
+	}
+
+	public String getCalle() {
+		return calle;
+	}
+
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+
+	public String getPob() {
+		return pob;
+	}
+
+	public void setPob(String pob) {
+		this.pob = pob;
+	}
+
+	public int getCp() {
+		return cp;
+	}
+
+	public void setCp(int cp) {
+		this.cp = cp;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Empresa getEmp() {
+		return emp;
+	}
+
+	public void setEmp(Empresa emp) {
+		this.emp = emp;
+	}
+
+}
